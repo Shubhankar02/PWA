cb3db044d646cc60dbd7940be536662e786e447e
Shubhankar02 <shborade@gmail.com>
2018-06-23 15:44:59 +0530

    dynamic

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,7 @@
 console.log('sw');
 
-const STATIC_CACHE = "1.1";
+const STATIC_CACHE = '1.1';
+const DYNAMIC_CACHE = '1.1'
 
 var urlsToCache =[
     '/',
@@ -15,7 +16,7 @@ var urlsToCache =[
     'resources/images/macroon.png',
     '/resources/images/icecream.png',
     '/resources/images/cookie.png',
-    '/resources/images/donut.png',
+    '/resources/images/donut.png'
 ];
 
 self.addEventListener('install',(event)=>{
@@ -41,25 +42,42 @@ self.addEventListener('install',(event)=>{
 //     )
 // })
 
-//offline page
-self.addEventListener('fetch',(event)=>{
-    event.respondWith(
-        caches.match(event.request)
-        .then((response)=>{
-            if(response){
-                return response;
-            }
-            else{
-                return fetch(event.request)
-                .then((response)=>{
-                    return response;
-                })
-            }
-        }).catch((err)=>{
-            return caches.open(STATIC_CACHE)
+// //offline page
+// self.addEventListener('fetch',(event)=>{
+//     event.respondWith(
+//         caches.match(event.request)
+//         .then((response)=>{
+//             if(response){
+//                 return response;
+//             }
+//             else{
+//                 return fetch(event.request)
+//                 .then((response)=>{
+//                     return response;
+//                 })
+//             }
+//         }).catch((err)=>{
+//             return caches.open(STATIC_CACHE)
+//             .then(function(cache){
+//                 return cache.match('/offline.html');
+//             })
+//         })
+//     )
+// })
+
+self.addEventListener('fetch', function(e){
+    var request = e.request;
+    e.respondWith(
+        fetch(request)
+        .then(function(res){
+            return caches.open(DYNAMIC_CACHE)
             .then(function(cache){
-                return cache.match('/offline.html');
+                cache.put(request.url, res.clone());
+                return res;
             })
+        })
+        .catch(function(err){
+            return caches.match(request);
         })
     )
 })
